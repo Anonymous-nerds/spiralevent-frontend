e90a218fd4ec6f6d6c11012b0921bf455309471f
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2024-12-01 13:19:04 +0100

    button changed

--- src/components/hero1.jsx
+++ src/components/hero1.jsx
@@ -7,7 +7,7 @@ const hero1 = () => {
       {/* Left Section */}
       <div className="bg-neutral-100 flex items-center h-screen w-full ">
         <div>
-        <div className=' ml-12'> 
+        <div className=' ml-12 '> 
             <h3 className='text-5xl sm:text-4xl lg:text-4xl tracking-wide text-lg md:text-5xl font-bold mr-6'>Spiral<span className='text-pink-900'>Event</span></h3>
             <p className='mb-2 ml-4 font-bold text-gray-700 sm:1xl'>
                 coming soon...
@@ -20,21 +20,21 @@ const hero1 = () => {
         <form>
           <div className='grid grid-cols-2 p-0 m-0 '>
             <div className='input-box'>
-              <label className='p-10'>First Name</label>
+              <label className='p-10 text-gray-800 font-bold'>First Name</label>
               <input type="text" className='field w-4/5' placeholder='First name' required/>
             </div>
 
             <div className='input-box'>
-              <label className='p-10'>Last Name</label>
+              <label className='p-10 text-gray-800 font-bold'>Last Name</label>
               <input type="text" className='field w-4/5' placeholder='Last Name' required/>
             </div>
             </div>
 
             <div className='input-box'>
-              <label className='p-20'>Email</label>
+              <label className='p-20 text-gray-800 font-bold'>Email</label>
               <input type="email" className='field w-3/4' placeholder='Enter Your Email' required/>
             </div>
-            <button type='submit'>Join Waitlist</button>
+            <button type='submit' className='bg-pink-900'>Join Waitlist</button>
           </form>
         </div>
         
