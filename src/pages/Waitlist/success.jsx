d6a6d0b95fb1d101d9b33ed2d04295d10985e04f
Beebarh.jy <153818039+Barbexa@users.noreply.github.com>
2024-12-07 11:05:03 +0100

    image

--- src/pages/Waitlist/success.jsx
+++ src/pages/Waitlist/success.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Success = () => {
   return (
-    <div className='w-full h-full p-10 '>
+    <div className='w-full h-full p-0'>
         <div className='flex justify-center items-center text-center'>
-            <h2 className='text-4xl font-semibold text-gray-900'>Success!</h2>
+            <h2 className='p-5 text-4xl font-semibold text-gray-900'>Success!</h2>
         </div>
-        <div className='justify-center items-center flex '>
-            <img src="./Ok-amico.png" alt="" className='flex h-[20rem] w-[30rem] justify-center items-center' />
+        <div className='justify-center items-center flex pb-[-20px]'>
+            <img src="./rb_26076.png" alt="" className='flex h-[300px] w-[20rem] object-contain justify-center items-center' />
         </div>
         <div>
-            <p className='pb-[10px]  text-gray-900'>We have sent you an email verification to <br />
+            <p className=' text-gray-900'>We have sent you an email verification to <br />
             <span className='text-gray-700'>barbex.jy@gmail.com</span> </p>
         </div>
 
-        <div>
-            <button className='w-[40%] h-[40px] bg-gray-900 text-[#ffff] rounded-[8px] shadow-md hover:bg-[#831843]'>PROCEED</button>
+        <div className='pt-[10px]'> 
+            <button className='w-[40%] h-[40px]  bg-gray-900 text-[#ffff] rounded-[8px] shadow-md'>PROCEED</button>
         </div>
 
     </div>
