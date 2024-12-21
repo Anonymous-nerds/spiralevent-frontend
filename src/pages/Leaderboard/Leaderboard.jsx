ee79bff5164dcfca22c313cee6cf6aca2008e8ae
anasyakubu <yakubuanas04@gmail.com>
2024-12-21 20:15:17 +0100

    added helmat

--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import LeaderboardCom from "../../components/LeaderboardCom";
+import { Helmet } from "react-helmet";
 
 const Leaderboard = () => {
   return (
     <div>
+      <Helmet>
+        <title>Leaderboard of Champions ~ Spiral Event</title>
+      </Helmet>
       <LeaderboardCom />
     </div>
   );
